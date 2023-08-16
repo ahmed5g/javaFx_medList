8b040f24418ad3d5663a7ac4c3b471df292cf87a
Ahmed Ghorbel <ahmed.ghorbel1@esprit.tn>
2023-08-16 12:59:39 +0100

    data compression to binary implimented

--- JavaFx Application/MedList/src/main/java/tn/esprit/medlist/FindLocation/JavaScriptBridge.java
+++ JavaFx Application/MedList/src/main/java/tn/esprit/medlist/FindLocation/JavaScriptBridge.java
@@ -4,15 +4,35 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+import java.util.Base64;
+import java.util.zip.Inflater;
+
 
 public class JavaScriptBridge {
-    public void receiveUpdatedData(Object data) {
+    public void receiveUpdatedData(String compressedBase64) {
         try {
+            // Decode the base64 compressed data
+            byte[] compressedData = Base64.getDecoder().decode(compressedBase64);
+
+            // Decompress the data using Inflater
+            Inflater inflater = new Inflater();
+            inflater.setInput(compressedData);
+
+            byte[] buffer = new byte[1024];
+            StringBuilder output = new StringBuilder();
+
+            while (!inflater.finished()) {
+                int count = inflater.inflate(buffer);
+                output.append(new String(buffer, 0, count));
+            }
+
+            inflater.end();
+
+            // Process the decompressed JSON data
             ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonData = objectMapper.readTree(data.toString());
+            JsonNode jsonData = objectMapper.readTree(output.toString());
 
             // Now you can work with the jsonData object
-            // For example, you can access properties using jsonData.get("propertyName")
             System.out.println("Received data from JavaScript: " + jsonData);
         } catch (Exception e) {
             e.printStackTrace();
